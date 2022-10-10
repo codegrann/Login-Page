eda3d6181268baacff2b648b5ae9b084f71a9a62
codegram <omondihills02@gmail.com>
2022-10-10 20:25:11 +0300

    add

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let div=document.getElementById("div")
 let reset=document.getElementById("reset");
-let email=document.getElementById("email");
+let email1=document.getElementById("email1");
+let email2=document.getElementById("email2");
 reset.addEventListener("click", submit);
 function submit(){
-    alert(`We have sent you a reset code to ${email.value}`);
+    alert(`We have sent you a reset code to ${email2.value}`);
 };
+//  email1.addEventListener("mouseup", capitalize);
+//  function capitalize(){
+    // email1.value.style.fontVariant="upper";
+//  }
