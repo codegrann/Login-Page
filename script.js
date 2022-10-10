4a0bb8da98ce0de44e5ff73b4cb7a979ed44c30a
codegram <omondihills02@gmail.com>
2022-10-10 19:00:57 +0300

    add

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let div=document.getElementsByTagName("div")
+let div=document.getElementById("div")
 let reset=document.getElementById("reset");
 let email=document.getElementById("email");
 reset.addEventListener("click", submit);
 function submit(){
-    alert(`We have sent you a reset code to ${email.value}`)
-    div.style.backgroundColor="black";
+    alert(`We have sent you a reset code to ${email.value}`);
+    // div.style.backgroundColor="black";
+    // div.style.display="none";
+    // setTimeout(500);
 };
