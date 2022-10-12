90f663eb72789efd7c463b076d32b178be716433
codegram <omondihills02@gmail.com>
2022-10-12 21:43:34 +0300

    add

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let email1=document.getElementById("email1");
 let email2=document.getElementById("email2");
 reset.addEventListener("click", submit);
 function submit(){
-    alert(`We have sent you a reset code to ${email2.value}`);
+   let confirm=confirm(`Confirm that ${email2.value} is your email`);
+    if(confirm.value=false){
+        document.write("You have cancelled the password reset");
+    }
+    else{
+        alert(`We have sent you a reset code to ${email2.value}`)
+    };
 };
 //  email1.addEventListener("mouseup", capitalize);
 //  function capitalize(){
