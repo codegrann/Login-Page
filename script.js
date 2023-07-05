eed16ce5ec1e3e3a3ab3e833fecca0156e9f4682
codegrann <omondihills02@gmail.com>
2023-07-05 07:27:18 +0300

    .

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let div=document.getElementById("div");
-let reset=document.getElementById("reset");
-let email1=document.getElementById("email1");
-let email2=document.getElementById("email2");
-let password=document.querySelector("#password");
-let togglePassword=document.querySelector("#togglePassword");
-
+let div = document.getElementById("div");
+let reset = document.getElementById("reset");
+let email1 = document.getElementById("email1");
+let email2 = document.getElementById("email2");
+let password = document.querySelector("#password");
+let togglePassword = document.querySelector("#togglePassword");
 
 reset.addEventListener("click", submit);
 togglePassword.addEventListener("click", toggle);
 
+function toggle(){
 
+  this.classList.toggle(".bigEye");
+}
 
-function submit(){
+function submit() {
   confirm(`Confirm that ${email2.value} is your email`);
-  alert(`We have sent you a reset code to ${email2.value}`)
-    
-   
+  alert(`We have sent you a reset code to ${email2.value}`);
 }
 
 //  email1.addEventListener("mouseup", capitalize);
 //  function capitalize(){
-    // email1.value.style.fontVariant="upper";
+// email1.value.style.fontVariant="upper";
 //  }
-
