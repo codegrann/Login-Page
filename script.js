85b285d52951e602e36838e9f7b84abe2638257c
codegrann <omondihills02@gmail.com>
2023-07-05 07:18:18 +0300

    selected password and toggler icon

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let div=document.getElementById("div")
+let div=document.getElementById("div");
 let reset=document.getElementById("reset");
 let email1=document.getElementById("email1");
 let email2=document.getElementById("email2");
+let password=document.querySelector('#password');
+let togglePassword=document.querySelector('#togglePassword');
+
+
 reset.addEventListener("click", submit);
+
+
+
 function submit(){
   confirm(`Confirm that ${email2.value} is your email`);
   alert(`We have sent you a reset code to ${email2.value}`)
@@ -14,3 +21,4 @@ function submit(){
 //  function capitalize(){
     // email1.value.style.fontVariant="upper";
 //  }
+
